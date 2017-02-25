da7b93995e7d7b1ef8664d1e0d1fbeaa7258701f
Bradley Rothenberg <BradRothenberg@gmail.com>
2017-02-25 12:52:58 -0500

    tesx file writer from rhino

--- Scripts/TESXv1.0.0_Rhino.py
+++ Scripts/TESXv1.0.0_Rhino.py
@@ -18,21 +18,22 @@ import System.Xml
 
 tesXVersion = "1.0.0"
 
+mat4XML = [[ "aa", "ab","ac","ad" ], [ "ba", "bb","bc","bd" ],[ "ca", "cb","cc","cd" ],[ "da", "db","dc","dd" ] ]
 
 def main():
     rulename = rs.GetString("Input Rule Name","New Rule")
-    parent1 = rs.GetObject("select Parent",16)
-    children1 = rs.GetObjects("Select children", 16)
+    parent = rs.GetObject("select Parent",16)
+    children = rs.GetObjects("Select children", 16)
 
     #create a filename variable
-    filename = rs.SaveFileName("Save CSV file","*.csv||", None, rulename, "csv")
+    #prompt the user to specify a file name
+    filter = "Tessellation XML File (*.tesx)|*.tesx|All files (*.*)|*.*||"
+    filename = rs.SaveFileName("Save Tessellation XML File As", filter)
+    if not filename: return
+    XMLExport(filename,rulename, parent, children)
 
-    #open the file for writing
-    file = open(filename, 'w')
 
-
-    brep = rs.coercebrep(parent1)
-
+def ExtractPtsFaces(brep):
     verts = brep.Vertices
     faces = brep.Faces
 
@@ -40,12 +41,6 @@ def main():
     for v in verts:
         loc = v.Location
         ptArray.append(loc)
-
-    file.write("vertices," + str(len(ptArray)))
-    file.write("\n")
-    for pt in ptArray:
-        file.write(str(pt))
-        file.write("\n")
 
     faceArray = []
     for f in faces:
@@ -59,154 +54,102 @@ def main():
                     faces.append(i)
                     break
         faceArray.append(faces)
+    return [ptArray, faceArray]
+
+def getTransform(parent, child):
+    pCntr = rs.SurfaceAreaCentroid(parent)[0]
+    pArea = rs.SurfaceArea(parent)
+
+    cArea = rs.SurfaceArea(child)
+    scaleVal =math.sqrt( cArea[0] / pArea[0])
+    s = rs.XformScale(scaleVal, pCntr)
+    cCntr = rs.SurfaceAreaCentroid(child)[0]
+    t = rs.XformTranslation(cCntr-pCntr)
+    matrixF = t
+    if scaleVal!=1:
+        matrixF = rs.XformMultiply(matrixF, s)
+    return matrixF
 
 
-    file.write("faces," + str(len(faceArray)))
-    file.write("\n")
+def WriteUnit(writer, brep):
+    unit = ExtractPtsFaces(brep)
+    ptArray = unit[0]
+    faceArray = unit[1]
 
-    for face in faceArray:
-        file.write(str(face))
-        file.write("\n")
-
-
-
-    pCntr1 = rs.SurfaceAreaCentroid(parent1)[0]
-    pArea1 = rs.SurfaceArea(parent1)
-
-    file.write("children_1," + str(len(children1)))
-    file.write("\n")
-
-    for child in children1:
-        areaC1 = rs.SurfaceArea(child)
-        scaleVal =math.sqrt( areaC1[0] / pArea1[0])
-        cntr = rs.SurfaceAreaCentroid(child)[0]
-        t = rs.XformTranslation(cntr-pCntr1)
-        matrixF = t
-
-        file.write(str(matrixF))
-        file.write("\n")
-
-    file.close()
-
-def XMLTest():
-    filename = r"C:\Users\Phoenix\Desktop\testWrite.tesx"
-    xmldoc = System.Xml.XmlDocument()
-    xmldoc.Load(filename)
-    items = xmldoc.SelectNodes("tessellation/units/unit")
-    for item in items:
-        print item.InnerText
-    XMLExport()
-
-def WriteUnit(writer):
     writer.WriteStartElement("unit")
-
     writer.WriteStartElement("vertices")
     #write vertices here:
-    writer.WriteStartElement("vertex")
-    writer.WriteAttributeString("x", "-0.5")
-    writer.WriteAttributeString("y", "-0.5")
-    writer.WriteAttributeString("z", "-0.5")
-    writer.WriteEndElement()
+    for pt in ptArray:
+        writer.WriteStartElement("vertex")
+        writer.WriteAttributeString("x", str(pt[0]))
+        writer.WriteAttributeString("y", str(pt[1]))
+        writer.WriteAttributeString("z", str(pt[2]))
+        writer.WriteEndElement()
     writer.WriteEndElement()
 
     #Write faces here:
     writer.WriteStartElement("faces")
-    #write vertices here:
-    writer.WriteStartElement("face")
-    writer.WriteStartElement("face_vert")
-    writer.WriteAttributeString("n", "3")
-    writer.WriteEndElement()
-    writer.WriteEndElement()
+    for face in faceArray:
+        #write each face here:
+        writer.WriteStartElement("face")
+        for f in face:
+            writer.WriteStartElement("face_vert")
+            writer.WriteAttributeString("n", str(f))
+            writer.WriteEndElement()
+        writer.WriteEndElement()
     writer.WriteEndElement()
 
     writer.WriteEndElement()
 
 
-def WriteUnits(writer):
+def WriteUnits(writer, brep):
     writer.WriteStartElement("units")
-    WriteUnit(writer)
+    WriteUnit(writer, brep)
     writer.WriteEndElement()
 
-def WriteRule(writer):
+def WriteRule(writer, name, parent, children):
     writer.WriteStartElement("rule")
-    writer.WriteAttributeString("name", "Hex Prism")
+    writer.WriteAttributeString("name", name)
 
     writer.WriteStartElement("parent")
-    writer.WriteAttributeString("unit", "3")
+    writer.WriteAttributeString("unit", "0")
     writer.WriteEndElement()
-
-    #Write faces here:
     writer.WriteStartElement("children")
-    #write vertices here:
-    writer.WriteStartElement("child")
-    writer.WriteAttributeString("unit", "1")
-    writer.WriteStartElement("mat4")
-    writer.WriteAttributeString("aa", "3.1")
-    writer.WriteEndElement()
+    for child in children:
+        xform = getTransform(parent,child)
+        writer.WriteStartElement("child")
+        writer.WriteAttributeString("unit", "0")
+        writer.WriteStartElement("mat4")
+        for i in range (0,4):
+            for j in range(0,4):
+                writer.WriteAttributeString(mat4XML[i][j], str(xform[j,i]))
+        writer.WriteEndElement()
+        writer.WriteEndElement()
     writer.WriteEndElement()
     writer.WriteEndElement()
 
-    writer.WriteEndElement()
-
-def WriteRules(writer):
+def WriteRules(writer,name, parent, children):
     writer.WriteStartElement("rules")
-    WriteRule(writer)
+    WriteRule(writer,name, parent, children)
     writer.WriteEndElement()
 
-def XMLExport():
-    #prompt the user to specify a file name
-    filter = "Tessellation XML File (*.tesx)|*.tesx|All files (*.*)|*.*||"
-    filename = rs.SaveFileName("Save Tessellation XML File As", filter)
-    if not filename: return
-
+def XMLExport(filename,rulename, parent, children):
     settings = System.Xml.XmlWriterSettings()
     settings.Indent = True
     writer = System.Xml.XmlWriter.Create (filename, settings)
 
     writer.WriteComment(".tesx file" +tesXVersion+" exported from Rhino")
-
     #Write an element (this one is the root).
     writer.WriteStartElement("tessellation")
-    WriteUnits(writer)
-    WriteRules(writer)
-#
-#    #Write the namespace declaration.
-#    writer.WriteAttributeString("xmlns", "bk",None , "urn:samples")
-#
-#    #Write the genre attribute.
-#    writer.WriteAttributeString("genre", "novel")
-#
-#    #Write the title.
-#    writer.WriteStartElement("title")
-#    writer.WriteString("The Handmaid's Tale")
-#    writer.WriteEndElement()
-#
-#    #Write the price.
-#    writer.WriteElementString("price", "19.95")
-#
-#    #Lookup the prefix and write the ISBN element.
-#    prefix = writer.LookupPrefix("urn:samples")
-#    writer.WriteStartElement(prefix, "ISBN", "urn:samples")
-#    writer.WriteString("1-861003-78")
-#    writer.WriteEndElement()
-#
-#    #Write the style element (shows a different way to handle prefixes).
-#    writer.WriteElementString("style", "urn:samples", "hardcover")
-#
-#    #Write the close tag for the root element.
-#    writer.WriteEndElement()
-#
-#    #Write the XML to file and close the writer.
-    writer.Flush()
+    writer.WriteAttributeString("name", rulename)
+
+    brep = rs.coercebrep(parent)
+
+    WriteUnits(writer,brep)
+    WriteRules(writer,rulename, parent, children)
+
     writer.Close()
-#
-#    writer = System.Xml.XmlTextWriter(filename, System.Text.UTF8Encoding(True, True))
-#    writer.WriteStartDocument()
-#    writer.WriteStartElement("car")
-#    writer.WriteElementString("name", "Jaguar")
-#    writer.WriteEndElement()
-#    writer.WriteEndDocument()
-#    writer.Close()
+
 
 if __name__=="__main__":
     main()
